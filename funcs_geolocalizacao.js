133c2d32d7d61e198551b10a5d2854df0ef730fc
Everton <81933167+tonn3r@users.noreply.github.com>
2026-07-16 17:57:44 -0300

    Update funcs_geolocalizacao.js

--- funcs_geolocalizacao.js
+++ funcs_geolocalizacao.js
@@ -416,7 +416,7 @@ function verificarEIncrementarCotaGoogle(apiKey, nomeChave) {
 window._promiseGoogleMaps = window._promiseGoogleMaps || null;
 
 function carregarSDKGoogleMaps(apiKey) {
-    console.log(`🔑 Tentando carregar Google Maps SDK com a chave: ${apiKey}`);
+    console.log(`Tentando carregar Google Maps SDK com a chave: ${apiKey}`);
     // 1. Se já carregou e a API de Directions está pronta, retorna true imediatamente
     if (window.google && window.google.maps && window.google.maps.DirectionsService) {
         return Promise.resolve(true);
